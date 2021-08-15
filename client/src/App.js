4031cb7d824dc98602fa22c85c86da25987b3632
Bartek <bartlomiej.proczkowski@gmail.com>
2021-08-15 15:38:50 +0200

    Web App Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 import VideoPlayer from "./components/VideoPlayer";
-import Notifications from "./components/Notifications";
 import Options from "./components/Options";
-import { makeStyles } from "@material-ui/styles";
+import Notifications from "./components/Notifications";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    borderRadius: 15,
+    borderRadius: "150px",
+    backgroundColor: "#d8dfeb",
     margin: "30px 100px",
     display: "flex",
     flexDirection: "row",
@@ -15,6 +17,10 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     width: "600px",
     border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
   },
   image: {
     marginLeft: "15px",
@@ -29,17 +35,18 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
   const classes = useStyles();
+
   return (
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h2" align="center">
-          Video Chat App
+          VidMe Chat
         </Typography>
       </AppBar>
-      <VideoPlayer />
       <Options>
         <Notifications />
       </Options>
+      <VideoPlayer />
     </div>
   );
 };
